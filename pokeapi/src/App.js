c295ec8060681993b02ea88ccc2c7a6dde810736
Kevin Luciano <78965771+kalg12@users.noreply.github.com>
2022-03-26 12:13:19 -0600

    API

--- pokeapi/src/App.js
+++ pokeapi/src/App.js
@@ -1,9 +1,22 @@
+import React, {useState} from "react";
 import Navbar from "./components/navbar/Navbar";
+import Home from "./components/home/Home";
 
 function App() {
+  const [pokemon, setPokemon] = useState([]);
+
+  React.useEffect(() => {
+    fetch("https://pokeapi.co/api/v2/pokemon/?limit=151")
+      .then(response => response.json())
+      .then(data => setPokemon(data.results))
+      .catch(error => console.log(error));
+      console.log(pokemon);
+  }, []);
+
   return (
     <div className="App">
       <Navbar />
+      <Home pokemon={pokemon} />
     </div>
   );
 }
